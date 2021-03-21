06fc52010458761fde3cabfadb90e2232136d69c
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-03-20 18:32:05 -0600

    added jikan api for anime

--- js/marvel.js
+++ js/marvel.js
@@ -2,9 +2,9 @@
 //hash d15e0441c607b3698ea81d884dbb0a63
 // url = "https://gateway.marvel.com:443/v1/public/characters?apikey=e96f679ac4fad530bea9f5dac9dae437";
 
-const marvel = {
+const jikan = {
     render: () => {
-        const url = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=e96f679ac4fad530bea9f5dac9dae437&hash=d15e0441c607b3698ea81d884dbb0a63";
+        const url = "https://api.jikan.moe/v3";
         
         fetch(url)
         .then(res => res.json())
@@ -15,4 +15,4 @@ const marvel = {
     }
 };
 
-marvel.render();
+jikan.render();
