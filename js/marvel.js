cd233df18182a7869e0caf4b2e5d973c484dfef7
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-03-20 22:57:58 -0600

    added burguer menu

--- js/marvel.js
+++ js/marvel.js
@@ -2,17 +2,26 @@
 //hash d15e0441c607b3698ea81d884dbb0a63
 // url = "https://gateway.marvel.com:443/v1/public/characters?apikey=e96f679ac4fad530bea9f5dac9dae437";
 
-const jikan = {
-    render: () => {
-        const url = "https://api.jikan.moe/v3";
+
+const jikan = () => {
+        const url = "https://api.jikan.moe/v3/search/anime?q=baki&page=1";
         
         fetch(url)
-        .then(res => res.json())
-        .then((json) => {
-            console.log(json)
-        })
+        .then(data => data.json())
+        .then(data => console.log(data))
+        .then(updateDom)
+    
 
-    }
 };
 
-jikan.render();
+function updateDom () {
+
+     //document.getElementById("anime-title").innerHTML = 
+     //" <h6>'${data.results}'</h6> " ;
+}
+
+jikan();
+
+
+
+//narutos id results[0].mal_id
