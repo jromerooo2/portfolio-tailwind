f48d5f765b1c81b69500529f064a2600622d8ce7
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-03-20 23:50:14 -0600

    ordered js file and deleted some html

--- js/marvel.js
+++ js/marvel.js
@@ -1,27 +1,18 @@
+    const jikan = () => {
+            const url = "https://api.jikan.moe/v3/search/anime?q=baki&page=1";
+            
+            fetch(url)
+            .then(data => data.json())
+            .then(data => console.log(data))
+            .then()
 
-//hash d15e0441c607b3698ea81d884dbb0a63
-// url = "https://gateway.marvel.com:443/v1/public/characters?apikey=e96f679ac4fad530bea9f5dac9dae437";
-
-
-const jikan = () => {
-        const url = "https://api.jikan.moe/v3/search/anime?q=baki&page=1";
-        
-        fetch(url)
-        .then(data => data.json())
-        .then(data => console.log(data))
-        .then(updateDom)
-    
-
-};
-
-function updateDom () {
-
-     //document.getElementById("anime-title").innerHTML = 
-     //" <h6>'${data.results}'</h6> " ;
-}
-
-jikan();
+    };
 
 
 
-//narutos id results[0].mal_id
+
+    jikan();
+
+
+
+    //narutos id results[0].mal_id
